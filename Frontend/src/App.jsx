25ea23d75046230871cd164d2ba51969131ad02c
Pradyumn <bt22csa057@iiitn.ac.in>
2024-07-02 09:37:19 +0530

    first commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,22 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './mycomponents/Header';
+import Home from './mycomponents/Home';
+import Form from './mycomponents/Form'; // Adjust path as per your actual structure
 
-import './App.css'
-import { DatePickerDemo } from './components/ui/datepicker'
-import Form from './mycomponents/Form'
-import Header from './mycomponents/Header'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import Home from './mycomponents/Home'
 function App() {
-  return <div >
-    <BrowserRouter>
-    <Routes>
-      <Route></Route>
-    </Routes>
-    <Header></Header>
-    <Home></Home>
-    </BrowserRouter>
-    {/* <Form ></Form> */}
-
-  </div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<Form />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
