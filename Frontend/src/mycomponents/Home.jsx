23c2c87503fa4184b0bb513f680177c1b4bf124e
Pradyumn <pradyumnshirsath1710@gmail.com>
2024-08-01 08:30:20 +0530

    Working on Home page

--- Frontend/src/mycomponents/Home.jsx
+++ Frontend/src/mycomponents/Home.jsx
@@ -1,4 +1,13 @@
+/* eslint-disable no-unused-vars */
 import { Button } from "../components/ui/button"
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+  } from "@/components/ui/carousel"
+  
 
 export default function Home(){
     return <div>
@@ -7,8 +16,8 @@ export default function Home(){
     </div>
 }
 function Hero_Header(){
-    return <div>
-        <div className="flex justify-around mx-14 mt-16  ">
+    return <div className="border-2 border-rose-500 ">
+        <div className="flex justify-around mx-14 mt-16 border-2 border-rose-500  ">
             <div className="pr-9 text-4xl ">Transform Your Space with Our Expert Cleaning Services</div>
             <div>
                 <div className="text-2xl">Professional and Reliable Cleaning Solutions for Every Need.</div>
@@ -18,6 +27,23 @@ function Hero_Header(){
                 </div>
             </div>
         </div>
-        <div className="flex justify-center m-60">slideshow</div>
+        <div className=" ml-14 mr-14 mt-3 border-2 border-rose-500 ">
+        <Carousel >
+            <CarouselContent>
+                <CarouselItem>
+                    <img src="public\Commercial and Office Cleaning Sydney.jpeg" alt="" />
+                </CarouselItem>
+                <CarouselItem>
+                    <img src="public\Commercial and Office Cleaning Sydney.jpeg" alt="" />
+                </CarouselItem>
+                <CarouselItem>
+                    <img src="public\Commercial and Office Cleaning Sydney.jpeg" alt="" />
+                </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+
+        </div>
     </div>
 }
