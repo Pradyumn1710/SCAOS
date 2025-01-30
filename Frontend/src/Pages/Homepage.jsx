952458b10e9fd775a2eebda05243b668f80077ba
pradyumn <bt22csa057@gmail.com>
2025-01-31 00:24:31 +0530

    After literally eradicating my entire codebase due to merging conflict

--- Frontend/src/Pages/Homepage.jsx
+++ Frontend/src/Pages/Homepage.jsx
@@ -1,10 +1,10 @@
-import Footer from "@/mycomponents/Homepage/Footer/"
 import Partner from "@/mycomponents/other/Partner"
 // import Testimonial from "@/mycomponents/Testimonial"
 import HeroHeader from "@/mycomponents/Homepage/HeroHeader"
 import Services from "@/mycomponents/other/Services"
 import Review from "@/mycomponents/Homepage/Review"
-import Quote from "@/mycomponents/Homepage/Quote"    
+import Quote from "@/mycomponents/Homepage/Quote"   
+import Footer from "@/mycomponents/Homepage/Footer" 
 
 export default function Homepage(){
     return <div>
